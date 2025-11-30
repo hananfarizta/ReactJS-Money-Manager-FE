bb432ad6aa7ddb66ccd879f65a595021e7e4ef93
hananfarizta <hananfariztaa@gmail.com>
2025-11-30 15:30:28 +0700

    feat/add project configuration

---
+++ src/util/axiosConfig.jsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+
+const axiosConfig = axios.create({
+  baseURL:
+    "https://springboot-money-manager-be-production.up.railway.app/api/v1.0",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// List of endpoints to exclude from adding the Authorization header
+const excludeEndpoints = [
+  "/auth/login",
+  "/auth/register",
+  "auth/activate",
+  "/status",
+  "/health",
+];
+
+// Request Interceptor
+axiosConfig.interceptors.request.use(
+  (config) => {
+    const shouldSkipToken = excludeEndpoints.some((endpoint) => {
+      config.url?.includes(endpoint);
+    });
+
+    if (!shouldSkipToken) {
+      const accessToken = localStorage.getItem("token");
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+      return config;
+    }
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response Interceptor
+axiosConfig.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error(
+          "Server Error. Please try again later",
+          error.response.data,
+        );
+      } else if (
+        error.code === "ECONNABORTED" ||
+        error.message === "Network Error"
+      ) {
+        console.error(
+          "Request timed out or Network Error. Please check your connection.",
+        );
+      }
+    }
+    return Promise.reject(error);
+  },
+);
